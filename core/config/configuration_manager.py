94ee2cd346036a23d37305f7a346e1e4870c5db7
mbruin <menno.bruin@student.uva.nl>
2021-11-22 15:23:58 +0100

    updated logging

--- core/config/configuration_manager.py
+++ core/config/configuration_manager.py
@@ -1,18 +1,33 @@
 import yaml
 import logging
-
-LOG = logging.getLogger(__name__)
+import sys
 
 
 class ConfigurationManager:
 
     def __init__(self, path):
         self.path = path
+        self.config = None
+        self.LOG = self.get_logger(__name__)
 
-    def load(self):
+    @staticmethod
+    def get_logger(name) -> logging.Logger:
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG)
+
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s  - %(name)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        return logger
+
+    def load_config(self):
+
         try:
             with open(self.path, 'r') as f:
-                return yaml.safe_load(f)
+                self.config = yaml.safe_load(f)
+                return self.config
         except IOError as e:
-            LOG.error(f"No configuration found at location {self.path}")
+            self.LOG.error(f"No configuration found at location {self.path}")
             raise e
