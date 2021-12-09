ca9b11aec6212c58fbde2ae4d7d87d160414e0c4
mennobruin <mennombruin@gmail.com>
2021-12-09 16:00:51 +0100

    updated data reader

--- application1/handler/data/reader.py
+++ application1/handler/data/reader.py
@@ -15,9 +15,9 @@ class DataReader:
         LOG.info(f"Loading {csv_file}")
         if not os.path.isfile(csv_file):
             csv_file = self.default_path + 'csv\\' + csv_file
-        else:
-            LOG.error(f"Unable to load csv_file: {csv_file}. Check if the file exists.")
-            raise FileNotFoundError
+            if not os.path.isfile(csv_file):
+                LOG.error(f"Unable to load csv_file: {csv_file}. Check if the file exists.")
+                raise FileNotFoundError
 
         with open(csv_file, 'r') as f:
             return pd.read_csv(f)
--- application1/main.py
+++ application1/main.py
@@ -1,40 +1,30 @@
 import time
 
-import matplotlib.pyplot as plt
-
 from gwpy.timeseries import TimeSeries
-from gwpy.detector.channel import ChannelList, Channel
+from virgotools.frame_lib import getChannel
 
-from handler.data.reader import DataReader
-from core.auth.auth_service import AuthenticationService
 from core.config.configuration_manager import ConfigurationManager
+from handler.data.decimator import Decimator
 
 LOG = ConfigurationManager.get_logger(__name__)
 
 
-def get_data(channel, t_start, t_stop, connection=None, verbose=False, frametype='raw'):
+def get_data(channel, t_start, t_stop, connection=None, verbose=False):
     LOG.info(f"Fetching data from {channel}...")
     t0 = time.time()
     if connection:
         data = TimeSeries.fetch(channel, t_start, t_stop, connection=connection, verbose=verbose)
     else:
-        data = TimeSeries.get(channel, t_start, t_stop, allow_tape=True, verbose=verbose, frametype=frametype)
+        data = getChannel(channel, t_start, t_stop).data
     LOG.info(f"Fetched data from {channel}, time elapsed: {time.time() - t0:.1f}s")
     return data
 
 
 def main(t_start, t_stop, channel=None):
-    reader = DataReader()
-    file_name = 'gspy_O3b_c090_blip.csv'
-    df = reader.load_csv(file_name)
 
-    gps_time = df["GPStime"].values[0]
-    duration = df["duration"].values[0]
-    print(gps_time)
-
-    data = get_data(channel, t_start=gps_time-duration/2, t_stop=gps_time+duration/2, frametype='trend')
-    plt.plot(range(len(data)), data, 'b-')
-    plt.show()
+    data = get_data(channel, t_start, t_stop)
+    decimator = Decimator()
+    data_50hz = decimator.decimate(data, target_frequency=50)
 
 
 if __name__ == '__main__':
