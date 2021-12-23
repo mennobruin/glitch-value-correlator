29f106e72db1d4498f67ad36f39e8854f4c7d86c
mennobruin <mennombruin@gmail.com>
2021-12-23 10:38:28 +0100

    updated params

--- application1/main.py
+++ application1/main.py
@@ -9,10 +9,11 @@ def main(t_start, t_stop, channel=None):
     decimator = Decimator()
 
     data = reader.get(channel, t_start, t_stop, source='raw_O3b_arch')
-    data_50hz = decimator.decimate(data, target_frequency=50)
+    # data_50hz = decimator.decimate(data, target_frequency=50)
+    print(data.shape)
 
 
 if __name__ == '__main__':
-    main(channel='V1:INJ_IMC_TRA_DC',
-         t_start=1171432800,
-         t_stop=1171450800)
+    main(channel='V1:Hrec_hoft_2_200Hz',
+         t_start=1262228600,
+         t_stop=1262228700)
