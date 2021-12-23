06136d44ceded6667aa1a6fea6b0d294ad6b205b
mennobruin <mennombruin@gmail.com>
2021-12-23 10:45:24 +0100

    added logging to decimate

--- application1/handler/data/decimator.py
+++ application1/handler/data/decimator.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 
 from core.config.configuration_manager import ConfigurationManager
@@ -11,16 +12,22 @@ class Decimator:
         pass
 
     def decimate(self, input_data: np.array, target_frequency=50, method='mean'):
-
         input_size = len(input_data)
+        LOG.info(f"Decimating {input_size} data points with target frequency {target_frequency}Hz...")
+        t0 = time.time()
+
         ds_ratio = input_size / target_frequency
 
         if not ds_ratio.is_integer():
             LOG.warning(f"Size of input data {input_size} is not integer divisible by target frequency {target_frequency}.")
         ds_ratio = int(ds_ratio)
 
+        output_data = None
         if method == 'mean':
-            return self._n_sample_average(input_data, ds_ratio)
+            output_data = self._n_sample_average(input_data, ds_ratio)
+
+        LOG.info(f"Decimating complete. Time elapsed: {time.time() - t0:.1f}s")
+        return output_data
 
     @staticmethod
     def _n_sample_average(x: np.array, n):
