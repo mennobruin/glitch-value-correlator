11978361161782b7e85cf6481829e04bd1e645a1
mennobruin <mennombruin@gmail.com>
2022-01-04 14:48:50 +0100

    added channels name fetching

--- application1/main.py
+++ application1/main.py
@@ -1,3 +1,5 @@
+from opencv import
+
 from core.config import ConfigurationManager
 from handler.data import Decimator, DataReader
 from model.channel import Channel
@@ -5,14 +7,18 @@ from model.channel import Channel
 LOG = ConfigurationManager.get_logger(__name__)
 
 
-def main(t_start, t_stop, channel_name=None):
+def main(channel_name, t_start, t_stop):
     reader = DataReader()
     decimator = Decimator()
 
-    channel: Channel = reader.get(channel_name, t_start, t_stop, source='raw_O3b_arch')
-    data_50hz = decimator.decimate(channel.x, input_frequency=channel.dx, target_frequency=50)
-    print(channel.x.shape)
-    print(data_50hz.shape)
+    available_channels = reader.get_available_channels(channel_name, t_start)
+    print(available_channels)
+
+    # channel1: Channel = reader.get(channel_name, t_start, t_stop, source='raw_O3b_arch')
+    # channel2: Channel = reader.get(channel_name, t_start, t_stop, source='raw_O3b_arch')
+    # data1_50hz = decimator.decimate(channel1.x, input_frequency=channel1.dx, target_frequency=50)
+    # data2_50hz = decimator.decimate(channel2.x, input_frequency=channel2.dx, target_frequency=50)
+
 
 
 if __name__ == '__main__':
