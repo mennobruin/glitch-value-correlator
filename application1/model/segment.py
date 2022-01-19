47ad183b6f4c0b28b92d4ceb1ec73727ebeb5fd6
mennobruin <mennombruin@gmail.com>
2022-01-19 11:58:56 +0100

    added properties to segment

--- application1/model/segment.py
+++ application1/model/segment.py
@@ -2,9 +2,12 @@
 
 class Segment:
 
-    def __init__(self, channel, x, f_sample, gps_time, unit=None):
+    def __init__(self, channel, x, dt, f_sample, gps_time, unit=None):
         self.channel = channel
         self.x = x
+        self.dt = dt
+        self.x_size = int(dt * f_sample)
         self.f_sample = f_sample
         self.gps_time = gps_time
         self.unit = unit
+        self.decimated = False
