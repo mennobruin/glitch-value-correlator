cf0eb1a65c759bc9f1b979f6e1b9e95a1aa9a658
mennobruin <mennombruin@gmail.com>
2022-01-19 11:35:44 +0100

    fixed source / channel name confusion

--- application1/main.py
+++ application1/main.py
@@ -1,25 +1,28 @@
-from model.channel import Channel
-from handler.data import Decimator, DataReader
-from .core.config import ConfigurationManager
+from fnmatch import fnmatch
+
+from application1.model.channel import Channel
+from application1.handler.data import Decimator, DataReader
+from core.config import ConfigurationManager
 
 LOG = ConfigurationManager.get_logger(__name__)
 
 
-def main(channel_name, t_start, t_stop):
+def main(source, channel_name, t_start, t_stop):
     reader = DataReader()
     decimator = Decimator()
 
-    available_channels = reader.get_available_channels(channel_name, t_start)
-    print(available_channels)
+    # available_channels = reader.get_available_channels(source, t_start)
 
-    # channel1: Channel = reader.get(channel_name, t_start, t_stop, source='raw_O3b_arch')
-    # channel2: Channel = reader.get(channel_name, t_start, t_stop, source='raw_O3b_arch')
-    # data1_50hz = decimator.decimate(channel1.x, input_frequency=channel1.dx, target_frequency=50)
-    # data2_50hz = decimator.decimate(channel2.x, input_frequency=channel2.dx, target_frequency=50)
-
+    channel1: Channel = reader.get(channel_name, t_start, t_stop, source=source)
+    data1_50hz = decimator.decimate(channel1.x, input_frequency=channel1.dx, target_frequency=50)
+    
+    print(channel1.gps_time)
+    print(channel1.dx)
+    print(len(channel1.x))
 
 
 if __name__ == '__main__':
-    main(channel_name='V1:Hrec_hoft_2_200Hz',
+    main(source='/virgoData/ffl/raw_O3b_arch',
+         channel_name='V1:Hrec_hoft_2_200Hz',
          t_start=1262228600,
          t_stop=1262228700)
