cf4b186d4ae5ad19f9a73aadc5f8f6edfae05ceb
mennobruin <mennombruin@gmail.com>
2022-01-19 12:03:01 +0100

    made x_size @property

--- application1/model/segment.py
+++ application1/model/segment.py
@@ -6,8 +6,11 @@ class Segment:
         self.channel = channel
         self.x = x
         self.dt = dt
-        self.x_size = int(dt * f_sample)
         self.f_sample = f_sample
         self.gps_time = gps_time
         self.unit = unit
         self.decimated = False
+
+    @property
+    def x_size(self):
+        return int(self.dt * self.f_sample)
