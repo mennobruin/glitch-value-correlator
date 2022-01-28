4e4d53dd8a96602c23c9433afe1ac5a2b3694b1a
mennobruin <mennombruin@gmail.com>
2022-01-28 09:57:12 +0100

    omicron trigger pipeline

---
+++ application1/handler/triggers/__init__.py
@@ -0,0 +1,1 @@
+from .omicron import Omicron
---
+++ application1/handler/triggers/omicron.py
@@ -0,0 +1,15 @@
+from subprocess import Popen, PIPE
+
+
+class Omicron:
+
+    COMMAND = '/virgoApp/Omicron/v2r3p11/Linux-x86_64-CL7/omicron-print.exe channel={0} gps-start={1:d} gps-end={2:d}'
+    FORMAT = [('gps', float), ('freq', float), ('snr', float)]
+
+    def __init__(self, channel):
+        self.channel = channel
+
+    def get_segment(self, gps_start, gps_end):
+        command = self.COMMAND.format(self.channel, gps_start, gps_end)
+        process = Popen(command, stdout=PIPE)
+
