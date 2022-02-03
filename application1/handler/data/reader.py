d40bb647ec5321e82e684f67e4143e0f25f0bd92
mennobruin <mennombruin@gmail.com>
2022-02-03 12:18:03 +0100

    changed \\ to / in paths

--- application1/handler/data/reader.py
+++ application1/handler/data/reader.py
@@ -15,7 +15,7 @@ LOG = ConfigurationManager.get_logger(__name__)
 
 class DataReader:
     def __init__(self):
-        self.default_path = str(pathlib.Path(__file__).parents[2].resolve()) + "\\resources\\"
+        self.default_path = str(pathlib.Path(__file__).parents[2].resolve()) + "/resources/"
 
     @staticmethod
     def get_channel(channel_name, t_start, t_stop, source='raw', connection=None, verbose=False) -> Segment:
@@ -62,7 +62,7 @@ class DataReader:
 
         LOG.info(f"Loading {csv_file}")
         if not os.path.isfile(csv_file):
-            csv_file = self.default_path + 'csv\\' + csv_file
+            csv_file = self.default_path + 'csv/' + csv_file
             if not os.path.isfile(csv_file):
                 LOG.error(f"Unable to load csv_file: {csv_file}. Check if the file exists.")
                 raise FileNotFoundError
