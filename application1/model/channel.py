ec4051209d852cc4c4b5909a179b081a1d5458eb
mennobruin <mennombruin@gmail.com>
2022-02-11 13:13:07 +0100

    allowed csv writing of Channel object

--- application1/model/channel.py
+++ application1/model/channel.py
@@ -6,3 +6,6 @@ class Channel:
         self.name = name
         self.f_sample = f_sample
         self.unit = unit
+
+    def __str__(self):
+        return f'{self.name}, {self.f_sample}, {self.unit}'
