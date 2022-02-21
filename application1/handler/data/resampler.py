8625b7e755fc0db6e9b011d57456c9b18caa1ec1
mennobruin <mennombruin@gmail.com>
2022-02-21 12:19:37 +0100

    typo

--- application1/handler/data/resampler.py
+++ application1/handler/data/resampler.py
@@ -61,9 +61,9 @@ class Resampler:
             padding.fill(np.nan)
             padded_data = np.append(data, padding)
             print(segment.duration)
-            ds_ratio = len(padded_data) / int(segment.duration) * self.f_target
+            ds_ratio = len(padded_data) / segment.duration * self.f_target
             print(ds_ratio)
-            segment.data = self._n_sample_average(padded_data, ratio=ds_ratio)
+            segment.data = self._n_sample_average(padded_data, ratio=int(ds_ratio))
         elif self.method == 'decimate':
             segment.data = self._decimate(segment)
         else:
