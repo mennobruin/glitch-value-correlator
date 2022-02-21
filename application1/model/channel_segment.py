53e7200669bb7ffa1dd13e3cb1a1724b448ab57e
mennobruin <mennombruin@gmail.com>
2022-02-21 12:16:02 +0100

    fixed channelsegment duration

--- application1/model/channel_segment.py
+++ application1/model/channel_segment.py
@@ -3,14 +3,11 @@ from .channel import Channel
 
 class ChannelSegment:
 
-    def __init__(self, channel: Channel, data, gps_time, duration):
+    def __init__(self, channel: Channel, data, gps_time):
         self.channel = channel
         self.data = data
         self.gps_start = gps_time
-        self.gps_end = gps_time + duration
-        self.duration = duration
+        self.n_points = len(self.data)
+        self.duration = int(self.n_points / self.channel.f_sample)  # duration in seconds
+        self.gps_end = gps_time + self.duration
         self.decimated = False
-
-    @property
-    def n_points(self):
-        return self.duration * self.channel.f_sample
