05f35fd46ce6444dffa04c698be784fff16d7453
mennobruin <mennombruin@gmail.com>
2022-02-22 14:53:42 +0100

    typo

--- plotting/decimate.py
+++ plotting/decimate.py
@@ -4,7 +4,7 @@ import pathlib
 import os
 
 
-resource_path = str(pathlib.Path(__file__).parents[0].resolve()) + '/application1/resources/'
+resource_path = str(pathlib.Path(__file__).parents[1].resolve()) + '/application1/resources/'
 ds_path = resource_path + 'ds_data/'
 data_path = ds_path + 'data/'
 
