8b52f3192282abbbdd3660fe66d28e4267859c84
mennobruin <mennombruin@gmail.com>
2022-02-24 09:37:58 +0100

    testing speed differences

---
+++ application1/test/speed_compare.py
@@ -0,0 +1,32 @@
+from fnmatch import fnmatch
+
+from application1.model.channel import Channel
+
+from virgotools.frame_lib import FrameFile
+
+source = '/virgoData/ffl/raw_O3b_arch'
+t_start = 1262230000
+t_stop = t_start + 100
+f_target = 50
+
+exclude_patterns = ['*max', '*min', 'V1:VAC*', 'V1:Daq*', '*rms']
+frame_file = FrameFile(source)
+
+
+def get_available_channels(t0):
+    with frame_file.get_frame(t0) as f:
+        channels = [Channel(name=str(adc.contents.name),
+                            f_sample=adc.contents.sampleRate)
+                    for adc in f.iter_adc()]
+        if exclude_patterns:
+            return [c for c in channels if not any(fnmatch(c.name, p) for p in exclude_patterns)]
+        else:
+            return channels
+
+
+channels = get_available_channels(t0=t_start)
+print(f'number of channels: {len(channels)}')
+
+channels = [c for c in channels if c.f_sample > f_target]
+print(f'number of channels (>=50Hz): {len(channels)}')
+
