b556db1e2e2428f43a621fa656f62c467bbd2e4d
mennobruin <mennombruin@gmail.com>
2022-03-09 09:58:55 +0100

    testing

--- application1/handler/data/reader/base.py
+++ application1/handler/data/reader/base.py
@@ -13,6 +13,7 @@ class BaseReader:
     def _check_path_exists(self, file_loc, file):
         LOG.info(f"Loading {file}")
         if not os.path.isfile(file):
+            print(self.default_path, file_loc, file)
             file = self.default_path + file_loc + file
             if not os.path.isfile(file):
                 LOG.error(f"Unable to load file: {file}, check if the file exists.")
