97d2810c0ee023387bef311e0e1bb068c4c4b882
mennobruin <mennombruin@gmail.com>
2022-03-09 09:26:41 +0100

    added get_data_from_segments to h5reader

--- application1/handler/data/reader/h5.py
+++ application1/handler/data/reader/h5.py
@@ -3,32 +3,37 @@ import h5py
 import os
 from ligo import segments
 
+from application1.model.channel import Channel
 from .base import BaseReader
 
 from application1.utils import check_extension, split_file_name, RESOURCE_PATH
+from core.config import ConfigurationManager
+
+LOG = ConfigurationManager.get_logger(__name__)
 
 
 class H5Reader(BaseReader):
 
-    RECORD_STRUCTURE = [('h5_file', str, 100), ('gps_start', int), ('gps_end', int)]
+    RECORD_STRUCTURE = [('file', str, 100), ('gps_start', int), ('gps_end', int)]
     H5_DIR = RESOURCE_PATH + 'ds_data/data/'
     H5 = '.h5'
 
     def __init__(self, gps_start, gps_end):
         super(H5Reader, self).__init__()
-        self.h5_file = None
+        self.h5_cache = None
         self.gps_start = gps_start
         self.gps_end = gps_end
-        self.h5_files = self._get_files()
+        self.h5_records = self._get_records()
+        self.h5_files = self.h5_records.file
         self.segments = segments.segmentlist(
             segments.segment(gs, ge) for gs, ge in
-            zip(self.h5_files.gps_start, self.h5_files.gps_end)
+            zip(self.h5_records.gps_start, self.h5_records.gps_end)
         )
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.h5_file.close()
+        self.h5_cache.close()
 
-    def _get_files(self):
+    def _get_records(self):
         h5_files = [f for f in os.listdir(self.H5_DIR) if f.endswith(self.H5)]
         records = []
         for file in h5_files:
@@ -41,19 +46,23 @@ class H5Reader(BaseReader):
     def load_h5(self, h5_file):
         h5_file = check_extension(h5_file, extension=self.H5)
 
-        if self.h5_file is None or os.path.dirname(self.h5_file.filename) is not h5_file:
+        if self.h5_cache is None or os.path.dirname(self.h5_cache.filename) is not h5_file:
             h5_file = self._check_path_exists(file_loc=self.H5_DIR, file=h5_file)
-            self.h5_file = h5py.File(h5_file, 'r')
+            LOG.info(f'loading {h5_file}')
+            self.h5_cache = h5py.File(h5_file, 'r')
 
     def get_channel(self, channel_name):
-        return self.h5_file[channel_name]
+        return self.h5_cache[channel_name]
 
-    def get_data_from_segments(self, request_segments):
+    def get_data_from_segments(self, request_segments, channel: Channel):
         request_segments = segments.segmentlist([request_segments]) & self.segments
 
+        all_data = []
         for seg in request_segments:
             i_segment = self.segments.find(seg)
-            segment = self.segments[i_segment]
             h5_file = self.h5_files[i_segment]
-            print(segment)
-            print(h5_file)
+            self.load_h5(h5_file)
+            all_data.append(self.h5_cache[channel.name])
+        return all_data
+
+
