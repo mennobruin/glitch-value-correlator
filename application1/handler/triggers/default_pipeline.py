c3034e8718318fd98ce92df15cf0bdadf3837d0d
mennobruin <mennombruin@gmail.com>
2022-03-09 11:35:56 +0100

    testing

--- application1/handler/triggers/default_pipeline.py
+++ application1/handler/triggers/default_pipeline.py
@@ -9,14 +9,18 @@ LOG = ConfigurationManager.get_logger(__name__)
 class DefaultPipeline:
 
     GPS_TIME = 'GPStime'
+    LABEL = 'label'
 
-    def __init__(self, trigger_file):
+    def __init__(self, trigger_file, trigger_type=None):
         self.reader = CSVReader()
-        self.triggers = self.load_triggers(trigger_file)
-        LOG.info(f'Found {self.triggers.shape[0]} triggers.')
+        self.triggers = self.load_triggers(trigger_file, trigger_type)
+        LOG.info(f'Found {self.triggers.shape[0]} triggers of type {trigger_type if trigger_type else "[all]"}.')
 
-    def load_triggers(self, path_to_csv):
-        sorted_triggers = self.reader.load_csv(path_to_csv, usecols=[self.GPS_TIME]).sort_values(self.GPS_TIME)
+    def load_triggers(self, path_to_csv, trigger_type):
+        triggers = self.reader.load_csv(path_to_csv, usecols=[self.GPS_TIME])
+        if trigger_type:
+            triggers = triggers.loc[triggers['label'] == trigger_type]
+        sorted_triggers = triggers.sort_values(self.GPS_TIME)
         return sorted_triggers.values.flatten()
 
     def get_segment(self, gps_start, gps_end) -> np.ndarray:
