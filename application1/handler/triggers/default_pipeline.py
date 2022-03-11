fd0c6ba0f02992314ee298eb455c592753723e6c
mennobruin <mennombruin@gmail.com>
2022-03-11 15:51:11 +0100

    removed print statements

--- application1/handler/triggers/default_pipeline.py
+++ application1/handler/triggers/default_pipeline.py
@@ -15,7 +15,6 @@ class DefaultPipeline:
         self.reader = CSVReader()
         self.trigger_type = trigger_type
         self.triggers = self.load_triggers(trigger_file)
-        print(len(self.triggers))
 
     def load_triggers(self, path_to_csv):
         triggers = self.reader.load_csv(path_to_csv, usecols=[self.GPS_TIME, self.LABEL])
@@ -28,6 +27,4 @@ class DefaultPipeline:
         i_start, i_end = np.searchsorted(self.triggers, (gps_start, gps_end))
         LOG.info(f'Found {i_end - i_start} triggers of type {self.trigger_type if self.trigger_type else "[all]"} '
                  f'from {gps_start} to {gps_end}.')
-        print(i_start, i_end)
-        print(i_end - i_start)
         return self.triggers[i_start:i_end]
