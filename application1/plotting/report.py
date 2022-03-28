86241ba17e015180065397b756bc779f654162b0
mennobruin <mennombruin@gmail.com>
2022-03-28 11:21:10 +0200

    setting up html report

---
+++ application1/plotting/report.py
@@ -0,0 +1,21 @@
+import webbrowser
+import os
+
+from resources.constants import REPORT_INDEX
+
+
+def run_html(filename=REPORT_INDEX):
+    webbrowser.open('file://' + os.path.realpath(filename))
+
+
+class HTMLReport:
+
+    def __init__(self):
+        pass
+
+    def generate_report(self):
+        pass
+
+
+if __name__ == '__main__':
+    run_html()
--- resources/constants.py
+++ resources/constants.py
@@ -2,3 +2,4 @@
 from pathlib import Path
 
 RESOURCE_PATH = Path(__file__).parent.__str__() + '/'
+REPORT_INDEX = RESOURCE_PATH + 'report/index.html'
