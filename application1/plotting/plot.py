d95fcb18c66f96c21c7e34dba4048044ed132b4c
mennobruin <mennombruin@gmail.com>
2022-03-29 09:46:21 +0200

    added logging

--- application1/plotting/plot.py
+++ application1/plotting/plot.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 from resources.constants import PLOT_DIR
+from application1.config import config_manager
+
+
+LOG = config_manager.get_logger(__name__)
 
 
 def plot_histogram(data, save=False):
@@ -8,6 +12,7 @@ def plot_histogram(data, save=False):
 
 def plot_channel(channel, data, data_type, save=False, score=None):
     f_name = f'{score}_{channel}+{data_type}'
+    LOG.info(f'Plotting {f_name}...')
 
     fig = plt.figure(figsize=(10, 8), dpi=200)
 
