86fbadc3f1cd0269b3ae6da0f4cf75b5f9c7e209
mennobruin <mennombruin@gmail.com>
2022-03-29 10:11:44 +0200

    added transformation to plot name

--- application1/plotting/plot.py
+++ application1/plotting/plot.py
@@ -10,18 +10,18 @@ def plot_histogram(data, save=False):
     pass
 
 
-def plot_channel(channel, data, data_type, save=False, score=None):
-    f_name = f'{score}_{channel}+{data_type}'
+def plot_channel(channel, transformation, data, data_type, save=False, score=None):
+    f_name = f'{score}_{channel}_{transformation}+{data_type}'
     LOG.info(f'Plotting {f_name}...')
 
-    fig = plt.figure(figsize=(10, 8), dpi=200)
+    fig = plt.figure(figsize=(10, 8), dpi=100)
 
     plt.title(f_name)
     plt.bar(data.xgrid, data.counts, width=data.span / data.nbin)
     plt.xlim([data.offset, data.offset + data.span])
 
     if save:
-        save_name = f'{score}_{data_type}'
+        save_name = f'{score}_{transformation}_{data_type}'
         fig.savefig(PLOT_DIR + save_name + '.png', dpi=fig.dpi)
     else:
         plt.show()
