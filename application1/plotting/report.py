63d123de8e7dae66d815436af80b3b3244683d6c
mennobruin <mennombruin@gmail.com>
2022-03-31 14:52:50 +0200

    testing

--- application1/plotting/report.py
+++ application1/plotting/report.py
@@ -8,15 +8,20 @@ from resources.constants import REPORT_INDEX
 
 class HTMLReport:
 
+    TEMP_FILE = 'resources/report/temp.html'
+
     def __init__(self, filename=REPORT_INDEX):
         self.filename = filename
         with open(REPORT_INDEX) as f:
             self.html = bs4.BeautifulSoup(f.read(), features='lxml')
 
     def run_html(self):
-        # webbrowser.open('file://' + os.path.realpath(self.filename))
         print(self.html.prettify())
-        webbrowser.open("text/html;base64," + base64.b64encode(str(self.html)))
+        webbrowser.open('file://' + os.path.realpath(self.TEMP_FILE))
+
+    def update_html(self):
+        with open(self.TEMP_FILE, 'w+') as f:
+            f.write(str(self.html.prettify()))
 
     def add_row_to_table(self, content: list, tag='td', table_class=None):
         table = self.html.find('table', class_=table_class)
