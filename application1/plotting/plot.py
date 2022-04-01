8d9381657ae48b147d6ac3b1156d3d96e83af69b
mennobruin <mennombruin@gmail.com>
2022-04-01 09:42:39 +0200

    testing plotting

--- application1/plotting/plot.py
+++ application1/plotting/plot.py
@@ -10,7 +10,7 @@ def plot_histogram(data, save=False):
     pass
 
 
-def plot_channel(channel, transformation, data, data_type, save=False, score=None):
+def plot_histogram(channel, transformation, histogram, data_type, save=False, score=None):
     transformation = transformation if transformation is not '' else 'none'
     f_name = f'{score}_{channel}_{transformation}+{data_type}'
     LOG.info(f'Plotting {f_name}...')
@@ -18,11 +18,33 @@ def plot_channel(channel, transformation, data, data_type, save=False, score=Non
     fig = plt.figure(figsize=(10, 8), dpi=100)
 
     plt.title(f_name)
-    plt.bar(data.xgrid, data.counts, width=data.span / data.nbin)
-    plt.xlim([data.offset, data.offset + data.span])
+    plt.bar(histogram.xgrid, histogram.counts, width=histogram.span / histogram.nbin)
+    plt.xlim([histogram.offset, histogram.offset + histogram.span])
 
     if save:
         save_name = f'{score}_{transformation}_{data_type}'
+        fig.savefig(PLOT_DIR + save_name + '.png', dpi=fig.dpi)
+    else:
+        plt.show()
+    plt.clf()
+
+
+def plot_histogram_cdf(histogram, channel, transformation, data_type, save=False, score=None, fig=None, return_fig=False):
+    transformation = transformation if transformation is not '' else 'none'
+    f_name = f'{score}_{channel}_{transformation}_cdf+{data_type}'
+    LOG.info(f'Plotting {f_name}...')
+
+    if not fig:
+        fig = plt.figure(figsize=(10, 8), dpi=100)
+
+    plt.plot(histogram.xgrid, histogram.cdf, label=data_type)
+    plt.legend(loc='upper left')
+
+    if return_fig:
+        return fig
+
+    if save:
+        save_name = f'{score}_{transformation}_{data_type}_cdf'
         fig.savefig(PLOT_DIR + save_name + '.png', dpi=fig.dpi)
     else:
         plt.show()
