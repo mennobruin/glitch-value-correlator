29d6c7dff96f2cdfe757a3965b882ece137eb471
mennobruin <mennombruin@gmail.com>
2022-04-26 09:58:17 +0200

    testing

--- application1/plotting/plot_decimate.py
+++ application1/plotting/plot_decimate.py
@@ -20,10 +20,11 @@ files = os.listdir(data_path)
 f = 'excavator_f50_gs1262649700_ge1262649800_mean.h5'  # files[3]
 with h5py.File(data_path + f, 'r') as hf:
     frames = list(hf.keys())
-    for c in channels:
+    # for c in channels:
+    for i in range(100, 110):
         fig, axes = plt.subplots(2, 1)
-        channel = frames[frames.index(c)]
-        # channel = frames[300]
+        # channel = frames[frames.index(c)]
+        channel = frames[i]
         frame = hf.get(channel)
         data = np.array(frame)
 
@@ -38,5 +39,6 @@ with h5py.File(data_path + f, 'r') as hf:
 
         axes[1].set_title(channel)
         axes[1].plot(range(len(data)), data)
-        plt.show()
+        plt.show(block=False)
+    plt.show()
 
