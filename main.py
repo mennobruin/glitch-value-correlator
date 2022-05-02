af4a59b0905d672fd2ce92e7e7feb438ffb388e3
mennobruin <mennombruin@gmail.com>
2022-05-02 17:54:07 +0200

    testing

--- main.py
+++ main.py
@@ -176,10 +176,13 @@ class Excavator:
                 continue
             seg_triggers = triggers[slice_triggers_in_segment(triggers, gps_start, gps_end)]
             half_duration = seg_triggers.duration / 2
-            trigger_times = zip(seg_triggers.GPStime - half_duration, seg_triggers.GPStime + half_duration)
+            trigger_offset = seg_triggers.GPStime - gps_start
+            trigger_times = zip(trigger_offset - half_duration, trigger_offset + half_duration)
             print(list(trigger_times))
-            trigger_times = np.ravel([list(range(np.floor(t0), np.ceil(t1))) for (t0, t1) in trigger_times])
-            self.i_trigger = np.floor((trigger_times - gps_start) * self.f_target).astype(np.int32)
+            trigger_times = np.ravel([np.arange(np.floor(t0), np.ceil(t1)) * self.f_target for (t0, t1) in trigger_times])
+            print(list(trigger_times))
+            self.i_trigger = np.floor(trigger_times).astype(np.int32)
+            print(self.i_trigger)
             for channel in tqdm(self.available_channels, position=0, leave=True, desc=f'{segment[0]} -> {segment[1]}'):
                 self.update_channel_histogram(i_segment, segment, channel)
             self.h5_reader.reset_cache()
