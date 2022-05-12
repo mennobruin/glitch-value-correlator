8859a701ee681286c9e1269f536c16d9120c671e
mennobruin <mennombruin@gmail.com>
2022-05-12 13:13:12 +0200

    checking available channels

--- application1/plotting/plot_triggers.py
+++ application1/plotting/plot_triggers.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 plt.rcParams['font.size'] = 16
 
 from application1.handler.data.reader.csv import CSVReader
+from application1.handler.data.reader.frame_file import FrameFileReader
 from resources.constants import RESOURCE_DIR
 from application1.handler.triggers import DefaultPipeline
 from virgotools.frame_lib import FrameFile
@@ -11,6 +12,7 @@ source = '/virgoData/ffl/raw_O3b_arch'
 file = RESOURCE_DIR + 'csv/GSpy_ALLIFO_O3b_0921_final.csv'
 RESULTS_DIR = 'results/'
 reader = CSVReader()
+ffl_reader = FrameFileReader()
 triggers = reader.load_csv(file)
 triggers = triggers.sort_values('snr', ascending=False)
 
@@ -47,22 +49,25 @@ def plot_trigger_times():
 
 def plot_trigger_spectrogram(channel, trigger_type, i=0):
     trigger = dfs[trigger_type].iloc[i]
-    print(trigger)
-    duration = trigger.duration
     trigger_time = trigger.GPStime
+    duration = trigger.duration
+    print(trigger_time, duration)
     t0 = trigger_time - 3 * duration
     t1 = trigger_time + 3 * duration
 
-    with FrameFile(source) as ff:
-        data = ff.getChannel(channel, t0, t1).data
+    channels = ffl_reader.get_available_channels(trigger_time)
+    print([c for c in channels if 'Hrec_hoft' in c])
 
-    fig, ax = plt.subplots()
-    plt.specgram(data, Fs=200)
-    plt.xlabel('Time')
-    plt.ylabel('Frequency')
-    ax.set_yscale('log', basey=2)
-    plt.title(channel)
-    plt.show()
+    # with FrameFile(source) as ff:
+    #     data = ff.getChannel(channel, t0, t1).data
+    #
+    # fig, ax = plt.subplots()
+    # plt.specgram(data, Fs=200)
+    # plt.xlabel('Time')
+    # plt.ylabel('Frequency')
+    # ax.set_yscale('log', basey=2)
+    # plt.title(channel)
+    # plt.show()
 
 
 # plot_trigger_density(trigger='Scattered_Light')
