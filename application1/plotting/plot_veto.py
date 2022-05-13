19e49ec707727a891de958a5119c147526d67beb
mennobruin <mennombruin@gmail.com>
2022-05-13 14:44:23 +0200

    testing

--- application1/plotting/plot_veto.py
+++ application1/plotting/plot_veto.py
@@ -63,8 +63,9 @@ def plot_trigger_hist():
         ad_results = sorted(fom_ad.scores.items(), key=lambda f: f[1].ad, reverse=True)
         winner = ad_results[0]
         channel, result = winner
-        print(np.sum(h_aux_cum[channel].counts))
-        print(np.sum(h_trig_cum[channel].counts))
+        h_trig = h_trig_cum[channel]
+        print(np.nonzero(h_trig.counts))
+        print(h_trig.xgrid)
     else:
         print(f'{test_file=} not found, check t_start/t_stop in config.yaml')
 
