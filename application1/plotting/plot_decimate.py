cb19eb4e86d12a730ea4adff5a9524fb41b66cb1
mennobruin <mennombruin@gmail.com>
2022-05-13 11:22:54 +0200

    ok, no dir then

--- application1/plotting/plot_decimate.py
+++ application1/plotting/plot_decimate.py
@@ -41,7 +41,7 @@ with h5py.File(data_path + f, 'r') as hf:
             plt.xlabel(f'Time since gps={gs} [s]')
             plt.ylabel('[arb. unit]')
             plt.title(channel)
-            plt.savefig(RESULTS_DIR + f'{channel}_{gs}.png', dpi=300, transparent=False, bbox_inches='tight')
+            plt.savefig(f'{channel}_{gs}.png', dpi=300, transparent=False, bbox_inches='tight')
 
         # axes[1].set_title(channel)
         # axes[1].plot(range(len(data)), data)
