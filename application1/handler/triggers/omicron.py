fecc72a252dc315678a289e2dc9036d16650b341
mennobruin <mennombruin@gmail.com>
2022-05-30 10:39:25 +0200

    testing omicron

--- application1/handler/triggers/omicron.py
+++ application1/handler/triggers/omicron.py
@@ -4,7 +4,7 @@ from subprocess import Popen, PIPE
 
 class Omicron:
 
-    COMMAND = '/virgoApp/Omicron/v2r3p11/Linux-x86_64-CL7/omicron-print.exe channel={0} gps-start={1:d} gps-end={2:d}'
+    COMMAND = 'omicron-print channel={0} gps-start={1:d} gps-end={2:d}'
     FORMAT = [('gps', float), ('freq', float), ('snr', float)]
 
     def __init__(self, channel):
@@ -15,8 +15,8 @@ class Omicron:
         process = Popen(command, stdout=PIPE, shell=True)
         triggers = np.loadtxt(process.stdout, dtype=self.FORMAT)
         triggers = triggers.view(dtype=(np.record, triggers.dtype), type=np.recarray)
+        print(triggers.shape)
         print(triggers)
         triggers.sort(order='gps')
         print(triggers)
         return triggers
-
