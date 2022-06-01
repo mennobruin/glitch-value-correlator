8b46c9036cdb7eb94b3e98a05bab5bd9c1a24b8e
mennobruin <mennombruin@gmail.com>
2022-06-01 10:52:32 +0200

    testing

--- application1/plotting/plot_hist.py
+++ application1/plotting/plot_hist.py
@@ -27,9 +27,11 @@ with open(test_file, 'rb') as pkf:
 # channel = "V1:DQ_BRMSMon_BRMS_ALL_MIC_AIRPLANE_ENV_TCS_CO2_WI_MIC"
 # i = available_channels.index(channel)
 i = 2
+channel = available_channels[i]
+transformation = transformation_names[0]
+plot_histogram(channel=channel, transformation=transformation, histogram=h_trig_cum[channel, transformation], data_type='trig', block=False)
+plot_histogram(channel=channel, transformation=transformation, histogram=h_aux_cum[channel, transformation], data_type='aux', block=False)
 
-plot_histogram(channel=available_channels[i], transformation=transformation_names[0], histogram=h_trig_cum[i], data_type='trig', block=False)
-plot_histogram(channel=available_channels[i], transformation=transformation_names[0], histogram=h_aux_cum[i], data_type='aux', block=False)
-
-plot_histogram(channel=available_channels[i], transformation=transformation_names[2], histogram=h_trig_cum[i], data_type='trig', block=False)
-plot_histogram(channel=available_channels[i], transformation=transformation_names[2], histogram=h_aux_cum[i], data_type='aux')
+transformation = transformation_names[2]
+plot_histogram(channel=channel, transformation=transformation, histogram=h_trig_cum[channel, transformation], data_type='trig', block=False)
+plot_histogram(channel=channel, transformation=transformation, histogram=h_aux_cum[channel, transformation], data_type='aux', block=False)
