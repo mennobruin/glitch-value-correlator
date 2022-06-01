c2ac3da9d7ee9e8075c61ea51e68db5dac39f5ef
mennobruin <mennombruin@gmail.com>
2022-06-01 11:20:36 +0200

    testing

--- application1/plotting/plot_hist.py
+++ application1/plotting/plot_hist.py
@@ -25,17 +25,18 @@ with open(test_file, 'rb') as pkf:
 
 # print([c for c in available_channels if "DQ" in c])
 # channel = "V1:DQ_BRMSMon_BRMS_ALL_MIC_AIRPLANE_ENV_TCS_CO2_WI_MIC"
-# i = available_channels.index(channel)
-i = 2
+channel = "V1:DQ_BRMSMon_NSIG_LVDT_100mHz_200mHz_Sa_NE_F0_X_50Hz"
+i = available_channels.index(channel)
+# i = 2
 channel = available_channels[i]
-transformation = transformation_names[0]
+# transformation = transformation_names[0]
 # plot_histogram(channel=channel, transformation=transformation, histogram=h_trig_cum[channel, transformation], data_type='trig')
 # plot_histogram(channel=channel, transformation=transformation, histogram=h_aux_cum[channel, transformation], data_type='aux')
-fig = plot_histogram_cdf(channel=channel, transformation=transformation, histogram=h_trig_cum[channel, transformation], data_type='trig', return_fig=True)
-plot_histogram_cdf(channel=channel, transformation=transformation, histogram=h_aux_cum[channel, transformation], data_type='aux', fig=fig)
+# fig = plot_histogram_cdf(channel=channel, transformation=transformation, histogram=h_trig_cum[channel, transformation], data_type='trig', return_fig=True)
+# plot_histogram_cdf(channel=channel, transformation=transformation, histogram=h_aux_cum[channel, transformation], data_type='aux', fig=fig)
 
-transformation = transformation_names[2]
+# transformation = transformation_names[2]
 # plot_histogram(channel=channel, transformation=transformation, histogram=h_trig_cum[channel, transformation], data_type='trig')
 # plot_histogram(channel=channel, transformation=transformation, histogram=h_aux_cum[channel, transformation], data_type='aux')
-fig = plot_histogram_cdf(channel=channel, transformation=transformation, histogram=h_trig_cum[channel, transformation], data_type='trig', return_fig=True)
-plot_histogram_cdf(channel=channel, transformation=transformation, histogram=h_aux_cum[channel, transformation], data_type='aux', fig=fig)
+# fig = plot_histogram_cdf(channel=channel, transformation=transformation, histogram=h_trig_cum[channel, transformation], data_type='trig', return_fig=True)
+# plot_histogram_cdf(channel=channel, transformation=transformation, histogram=h_aux_cum[channel, transformation], data_type='aux', fig=fig)
