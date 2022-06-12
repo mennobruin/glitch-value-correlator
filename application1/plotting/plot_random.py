afd62516138c96ae2bf4e3d38c9dba5ec41104f3
mennobruin <mennombruin@gmail.com>
2022-06-12 20:54:02 +0200

    testing

--- application1/plotting/plot_random.py
+++ application1/plotting/plot_random.py
@@ -29,6 +29,9 @@ with open(test_file, 'rb') as pkf:
     h_aux_cum = data['aux']
     available_channels = data['channels']
 
+print(h_aux_cum)
+print(h_trig_cum)
+
 pipeline = LocalPipeline(trigger_file='GSpy_ALLIFO_O3b_0921_final.csv')
 labels = list(pipeline.labels)
 channel = 'V1:ENV_WEB_SEIS_W'
