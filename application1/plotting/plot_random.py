05d6d15a893643e6b5db8edd7ac042b650f3b7f7
mennobruin <mennombruin@gmail.com>
2022-06-12 20:31:37 +0200

    plotting

--- application1/plotting/plot_random.py
+++ application1/plotting/plot_random.py
@@ -59,9 +59,11 @@ fig = plt.figure(figsize=(10, 8), dpi=300)
 ax1 = fig.gca()
 ax2 = ax1.twinx()
 
-ax1.plot(h1_cp.xgrid[::-1], 100 * h1.cdf, '-', label="trig",)
-ax2.plot(h1_cp.xgrid[::-1], 100 * (1-h2.cdf), '-', label="aux",)
-plt.plot(h1_cp.xgrid[::-1], 100 * ((1-h2.cdf) - (1-h1.cdf)), '-', label=r"$\Delta$")
+p1 = ax1.plot(h1_cp.xgrid[::-1], 100 * h1.cdf, 'b-', label="trig",)
+p2 = ax2.plot(h1_cp.xgrid[::-1], 100 * (1-h2.cdf), 'g-', label="aux",)
+ax1.yaxis.label.set_color(p1.get_color())
+ax2.yaxis.label.set_color(p2.get_color())
+plt.plot(h1_cp.xgrid[::-1], 100 * ((1-h2.cdf) - (1-h1.cdf)), 'o-', label=r"$E_{\Delta}$")
 plt.xlim(min(h1_cp.xgrid), max(h1_cp.xgrid))
 plt.xlabel("x")
 ax1.set_ylabel("% vetoed")
@@ -70,41 +72,3 @@ plt.legend()
 plt.title(channel)
 save_name = f'veto_{channel}_{transformation_name}.png'
 fig.savefig(PLOT_DIR + save_name, dpi=fig.dpi)
-
-
-fig = plt.figure(figsize=(10, 8), dpi=300)
-ax1 = fig.gca()
-ax2 = ax1.twinx()
-
-x_mean = (h1.xgrid[0] + h1.xgrid[-1]) / 2
-x_new = h1.xgrid - x_mean
-
-ax1.plot(x_new[::-1], 100 * h1.cdf, '-', label="trig",)
-ax2.plot(x_new[::-1], 100 * (1-h2.cdf), '-', label="aux",)
-plt.plot(x_new[::-1], 100 * ((1-h2.cdf) - (1-h1.cdf)), '-', label=r"$\Delta$")
-plt.xlim(min(x_new), max(x_new))
-plt.xlabel("x")
-ax1.set_ylabel("% vetoed")
-ax2.set_ylabel("% DT")
-plt.legend()
-plt.title(channel)
-save_name = f'transformed_veto_{channel}_{transformation_name}.png'
-fig.savefig(PLOT_DIR + save_name, dpi=fig.dpi)
-
-plt.clf()
-plt.plot(x_new, h1.cdf)
-plt.xlim(min(x_new), max(x_new))
-plt.show()
-
-cdf1 = h1.cdf
-middle = len(x_new) // 2
-left, right = x_new[:middle], x_new[middle:]
-cdf_left = cdf1[:middle]
-cdf_right = cdf1[middle:]
-
-x_combined = right + abs(left[::-1])
-cdf_combined = cdf_right + cdf_left[::-1]
-
-plt.plot(x_combined, cdf_combined)
-plt.xlim(min(x_combined), max(x_combined))
-plt.show()
