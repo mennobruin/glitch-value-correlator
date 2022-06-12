7a1528fe5cbe59d1910b08e3436ece16aa1744ed
mennobruin <mennombruin@gmail.com>
2022-06-12 17:36:55 +0200

    plotting

--- application1/plotting/plot_random.py
+++ application1/plotting/plot_random.py
@@ -1,9 +1,11 @@
+import pickle
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 import copy as cp
 
-
+from application1.handler.triggers import LocalPipeline
 from application1.model.histogram import Hist
 from application1.plotting.plot import plot_histogram_cdf, plot_histogram
 from resources.constants import PLOT_DIR
@@ -15,10 +17,27 @@ def find_nearest_index(array, value):
     return np.abs(array - value).argmin()
 
 
-x1 = np.random.normal(loc=0, scale=1, size=10000)
-x2 = np.random.normal(loc=1, scale=1, size=200)
-h1 = Hist(x1, l2_nbin=10)
-h2 = Hist(x2, l2_nbin=10)
+# x1 = np.random.normal(loc=0, scale=1, size=10000)
+# x2 = np.random.normal(loc=1, scale=1, size=200)
+# h1 = Hist(x1, l2_nbin=10)
+# h2 = Hist(x2, l2_nbin=10)
+
+test_file = f'test_1264625000_1264635000_f50.pickle'
+with open(test_file, 'rb') as pkf:
+    data = pickle.load(pkf)
+    h_trig_cum = data['trig']
+    h_aux_cum = data['aux']
+    available_channels = data['channels']
+
+pipeline = LocalPipeline(trigger_file='GSpy_ALLIFO_O3b_0921_final.csv')
+labels = list(pipeline.labels)
+channel = 'V1:ENV_WEB_SEIS_W'
+transformation_name = ''
+h1 = h_aux_cum[channel, transformation_name]
+h2 = Hist(np.array([]))
+for label in labels:
+    h2 += h_trig_cum[channel, transformation_name, label]
+
 h1_cp = cp.deepcopy(h1)
 h2_cp = cp.deepcopy(h2)
 h1_cp += h2_cp
