a639ed59c3d6624b7b899b3dd22cc03ee2c50fef
mennobruin <mennombruin@gmail.com>
2022-06-20 12:23:44 +0200

    plotting

--- application1/plotting/plot_scattered_light.py
+++ application1/plotting/plot_scattered_light.py
@@ -30,44 +30,70 @@ with FrameFile(source) as ff:
     for t in tqdm(triggers.GPStime):
         p = ff.getChannel(channel, t, 1.2/f_sample).data
         points.append(abs(p - mean))
+points = np.array(points)
 
+i_scattered_light = np.flatnonzero(triggers[triggers.label == 'Scattered_Light'])
+i_other = np.flatnonzero(triggers[triggers.label != 'Scattered_Light'])
 
 plt.rcParams['font.size'] = 16
 
+# fig = plt.figure(figsize=(10, 8))
+# sc = plt.scatter(points, triggers.peakFreq, c=np.log10(triggers.snr), s=5*np.log10(triggers.snr))
+# plt.colorbar(sc)
+# plt.xlabel('Velocity [m/s]')
+# plt.ylabel('Trigger Frequency [Hz]')
+# plt.xlim(0, 1.15*max(points))
+# plt.ylim(0, 150)
+# save_name = f'{channel}_peakFreq.png'
+# fig.savefig(PLOT_DIR + save_name, dpi=fig.dpi)
+#
+# fig = plt.figure(figsize=(10, 8))
+# sc = plt.scatter(points, triggers.centralFreq, c=np.log10(triggers.snr), s=5*np.log10(triggers.snr))
+# plt.colorbar(sc)
+# plt.xlabel('Velocity [m/s]')
+# plt.ylabel('Trigger Frequency [Hz]')
+# plt.xlim(0, 1.15*max(points))
+# plt.ylim(0, 150)
+# save_name = f'{channel}_centralFreq.png'
+# fig.savefig(PLOT_DIR + save_name, dpi=fig.dpi)
+#
+# fig = plt.figure(figsize=(10, 8))
+# sc = plt.scatter(points, triggers.peakFreq, c=np.log10(triggers.snr), s=5*np.log10(triggers.snr))
+# plt.colorbar(sc)
+# plt.xlabel('Velocity [m/s]')
+# plt.ylabel('Trigger Frequency [Hz]')
+# plt.xlim(0, 1.15*max(points))
+# save_name = f'density_{channel}_peakFreq.png'
+# fig.savefig(PLOT_DIR + save_name, dpi=fig.dpi)
+#
+# fig = plt.figure(figsize=(10, 8))
+# sc = plt.scatter(points, triggers.centralFreq, c=np.log10(triggers.snr), s=5*np.log10(triggers.snr))
+# plt.colorbar(sc)
+# plt.xlabel('Velocity [m/s]')
+# plt.ylabel('Trigger Frequency [Hz]')
+# plt.xlim(0, 1.15*max(points))
+# save_name = f'density_{channel}_centralFreq.png'
+# fig.savefig(PLOT_DIR + save_name, dpi=fig.dpi)
+
+p_scattered_light = points[i_scattered_light]
+t_scattered_light = triggers[i_scattered_light]
+p_other = points[i_scattered_light]
+t_other = triggers[i_scattered_light]
+
 fig = plt.figure(figsize=(10, 8))
-sc = plt.scatter(points, triggers.peakFreq, c=np.log10(triggers.snr), s=5*np.log10(triggers.snr))
-plt.colorbar(sc)
+plt.scatter(p_other, t_other.peakFreq, s=5*np.log10(t_other.snr), a=0.7)
+plt.scatter(p_scattered_light, t_scattered_light.peakFreq, s=5*np.log10(t_scattered_light.snr))
 plt.xlabel('Velocity [m/s]')
 plt.ylabel('Trigger Frequency [Hz]')
 plt.xlim(0, 1.15*max(points))
-plt.ylim(0, 150)
-save_name = f'{channel}_peakFreq.png'
+save_name = f'{channel}_seperate_peakFreq.png'
 fig.savefig(PLOT_DIR + save_name, dpi=fig.dpi)
 
 fig = plt.figure(figsize=(10, 8))
-sc = plt.scatter(points, triggers.centralFreq, c=np.log10(triggers.snr), s=5*np.log10(triggers.snr))
-plt.colorbar(sc)
+plt.scatter(p_other, t_other.centralFreq, s=5*np.log10(t_other.snr), a=0.7)
+plt.scatter(p_scattered_light, t_scattered_light.centralFreq, s=5*np.log10(t_scattered_light.snr))
 plt.xlabel('Velocity [m/s]')
 plt.ylabel('Trigger Frequency [Hz]')
 plt.xlim(0, 1.15*max(points))
-plt.ylim(0, 150)
-save_name = f'{channel}_centralFreq.png'
-fig.savefig(PLOT_DIR + save_name, dpi=fig.dpi)
-
-fig = plt.figure(figsize=(10, 8))
-sc = plt.scatter(points, triggers.peakFreq, c=np.log10(triggers.snr), s=5*np.log10(triggers.snr))
-plt.colorbar(sc)
-plt.xlabel('Velocity [m/s]')
-plt.ylabel('Trigger Frequency [Hz]')
-plt.xlim(0, 1.15*max(points))
-save_name = f'density_{channel}_peakFreq.png'
-fig.savefig(PLOT_DIR + save_name, dpi=fig.dpi)
-
-fig = plt.figure(figsize=(10, 8))
-sc = plt.scatter(points, triggers.centralFreq, c=np.log10(triggers.snr), s=5*np.log10(triggers.snr))
-plt.colorbar(sc)
-plt.xlabel('Velocity [m/s]')
-plt.ylabel('Trigger Frequency [Hz]')
-plt.xlim(0, 1.15*max(points))
-save_name = f'density_{channel}_centralFreq.png'
+save_name = f'{channel}_seperate_centralFreq.png'
 fig.savefig(PLOT_DIR + save_name, dpi=fig.dpi)
